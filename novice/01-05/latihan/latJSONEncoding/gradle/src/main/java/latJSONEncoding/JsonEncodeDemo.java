e45814cb7776081b4e707224bbcb7f6fca3cbc0d
dedigunawan0123 <dedigunawan0123@gmail.com>
2019-12-09 15:53:50 +0700

    Menambahkan JSON

---
+++ novice/01-05/latihan/latJSONEncoding/gradle/src/main/java/latJSONEncoding/JsonEncodeDemo.java
@@ -0,0 +1,17 @@
+package main.java.latJSONEncoding;
+
+import org.json.simple.JSONObject;
+
+public class JsonEncodeDemo {
+
+   public void jsonEncodeDemoTest() {
+      JSONObject obj = new JSONObject();
+
+      obj.put("name", "foo");
+      obj.put("num", new Integer(100));
+      obj.put("balance", new Double(1000.21));
+      obj.put("is_vip", new Boolean(true));
+
+      System.out.print(obj);
+   }
+}

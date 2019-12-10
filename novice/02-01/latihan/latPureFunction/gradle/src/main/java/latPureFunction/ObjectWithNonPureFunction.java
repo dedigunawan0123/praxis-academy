56f7cfdc029fdf64c8033b2ed761c3a46fcddb95
dedigunawan0123 <dedigunawan0123@gmail.com>
2019-12-10 10:56:06 +0700

    Menambahkan JSON

---
+++ novice/02-01/latihan/latPureFunction/gradle/src/main/java/latPureFunction/ObjectWithNonPureFunction.java
@@ -0,0 +1,11 @@
+package main.java.latPureFunction;
+
+public class ObjectWithNonPureFunction{
+    private int value = 0;
+
+    public int add(int nextValue) {
+        this.value += nextValue;
+        return this.value;
+    }
+}
+
---
+++ novice/02-01/latihan/latPureFunction/gradle/src/main/java/latPureFunction/ObjectWithPureFunction.java
@@ -0,0 +1,8 @@
+package main.java.latPureFunction;
+
+public class ObjectWithPureFunction{
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}

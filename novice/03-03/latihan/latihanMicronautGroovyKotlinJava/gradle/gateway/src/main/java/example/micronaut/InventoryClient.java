dbba00a7772826edd640bf8c9615cfba70b1684b
dedigunawan0123 <dedigunawan0123@gmail.com>
2019-12-18 16:19:09 +0700

    Menambahkan MicroService

---
+++ novice/03-03/latihan/latihanMicronautGroovyKotlinJava/gradle/gateway/src/main/java/example/micronaut/InventoryClient.java
@@ -0,0 +1,18 @@
+package example.micronaut;
+
+import io.micronaut.context.annotation.Requires; 
+import io.micronaut.context.env.Environment; 
+import io.micronaut.http.annotation.Get; 
+// import io.micronaut.http.client.Client; 
+
+// dg. Mengganti pakai annotation
+import io.micronaut.http.client.annotation.Client;
+import io.reactivex.Maybe; 
+
+@Client("inventory") 
+@Requires(notEnv = Environment.TEST)
+public interface InventoryClient extends InventoryFetcher {
+    @Override 
+    @Get("/api/inventory/{isbn}") 
+    Maybe<Integer> inventory(String isbn);
+}
